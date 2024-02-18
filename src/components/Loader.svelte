0c21be8c7014753b7a5f966689398078074a4b28
sahil-raj <sahilrajaaru@gmail.com>
2024-02-18 23:48:53 +0530

    commit

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -1,14 +1,21 @@
 <script>
     export let loadPercentage = 10;
+    let currPercentage = 0;
 </script>
 
 <div>
     <div class="loader-body">
         <div class="loader" style="width: {loadPercentage}%;"></div>
+        <p class="load-per">{loadPercentage}%</p>
     </div>
+    <h4>working on it...</h4>
 </div>
 
 <style>
+    * {
+        text-align: center;
+    }
+
     .loader-body {
         height: 3vh;
         width: 50vw;

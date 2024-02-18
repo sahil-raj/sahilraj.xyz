16c1300dd7b035060f332067ca5318e104a56d2f
sahil-raj <sahilrajaaru@gmail.com>
2024-02-18 18:54:42 +0530

    vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import App from './App.svelte';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
 
 const app = new App({
 	target: document.body

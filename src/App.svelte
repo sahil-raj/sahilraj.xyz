2397658ff58e0cf174898cbd0a7f69a2bdd17d19
sahil-raj <sahilrajaaru@gmail.com>
2024-02-19 03:09:03 +0530

    start_commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,8 @@
 <script>
-    import TextData from "./components/TextData.svelte";
-	import Loader from "./components/Loader.svelte";
-
-	export let loadPercentage = 0;
-	export let statusText = "UC";
 </script>
 
 <main>
-	<div class="container-fluid my-cont">
-		<TextData/>
-		<Loader statusText={statusText} loadPercentage={loadPercentage}/>
-	</div>
 </main>
 
 <style>
-	.my-cont {
-		height: 100vh;
-		width: 100vw;
-		background: #000;
-		position: fixed !important;
-		overflow-y: scroll;
-	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		loadPercentage: 25,
-		statusText: "working on it..."
-	}
+	target: document.body
 });
 
 export default app;

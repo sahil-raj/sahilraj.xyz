9844c226f01d711553184b997afd7ccfe4c6420b
sahil-raj <sahilrajaaru@gmail.com>
2024-02-19 13:23:39 +0530

    changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,21 @@
-<script>
-</script>
-
 <main>
+    <div class="container-fluid">
+        <h1 id="main-h1">SAHIL RAJ</h1>
+    </div>    
 </main>
 
 <style>
+    .container-fluid {
+        height: 100vh;
+        width: 100vw;
+        background-color: rgba(0, 0, 0, 0.7);
+        margin: 0 !important;
+        position: absolute;
+    }
+
+    #main-h1 {
+        text-align: center;
+        font-size: 10vw;
+        margin-top: 40vh;
+    }
 </style>

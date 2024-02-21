3342fff876f6c0ebb6ccd19d32b6a41427ce681c
sahil-raj <sahilrajaaru@gmail.com>
2024-02-21 17:05:46 +0530

    css changes

--- src/components/MoveDown.svelte
+++ src/components/MoveDown.svelte
@@ -1,42 +1,48 @@
-<script></script>
+<script>
+    document.body.onscroll = (e) => {
+        // console.log(e);
+    }
+</script>
 
-<div class="main-move-down">
-    <div class="main-div-holder">
-        <div class="div-mover"></div>
+<div>
+    <div class="holder-all">
+        <div class="move-down-holder">
+            <div class="mover"></div>
+        </div>
+        <p>KNOW MORE</p>
     </div>
-    <p>KNOW MORE</p>
 </div>
 
 <style>
-    .main-move-down {
+    .holder-all {
         position: absolute;
-        width: 100%;
         text-align: center;
-        top: 0;
+        width: 100%;
+        top: 80vh;
+        padding: 0 !important;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
     }
 
-    p {
-        position: relative !important;
-        top: 82vh;
-    }
-
-    .main-div-holder {
+    .move-down-holder {
         height: 10vh;
         width: 1.5rem;
         border: 2px solid rgba(255, 255, 255, 0.5);
-        position: relative !important;
-        z-index: 9;
         border-radius: 999px;
-        left: 50%;
-        top: 80vh;
     }
 
-    .div-mover {
-        z-index: 9;
+    .mover {
         height: 1.5rem;
         width: 100%;
+        border-radius: 50%;
         background-color: rgba(0, 0, 255, 0.5);
-        border-radius: 100%;
         filter: blur(5px);
+        margin-top: calc(10vh - 1.5rem) !important;
+    }
+
+    p {
+        padding-top: 2vh;
     }
 </style>

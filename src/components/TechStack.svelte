9436eccf3c7cdb78b6e0aa16853712b13fba7b2e
sahil-raj <sahilrajaaru@gmail.com>
2024-02-26 05:18:35 +0530

    changes

--- src/components/TechStack.svelte
+++ src/components/TechStack.svelte
@@ -10,9 +10,9 @@
         class="d-flex flex-row flex-wrap justify-content-center align-item-center"
     >
         {#each techStackData as tech (tech.id)}
-            <div class="card border-none mb-3 mx-5" style="max-width: 18rem;">
-                <img src={tech.img_url} class="card-img-top" alt={tech.name} />
-                <div class="card-body text-white">
+            <div class="card border-none mb-3 mx-5 w-50" style="max-width: 18rem;">
+                <i class="card-img-top rounded p-3 lni {tech.img_url}"></i>
+                <div class="card-body text-black">
                     <h5 class="card-title">{tech.name}</h5>
                 </div>
             </div>
@@ -25,14 +25,20 @@
 <style>
     .tech-stack-wrapper {
         background-color: #4fc3f7;
+        overflow-x: hidden;
     }
 
     .tech-stack-wrapper > h1 {
-        font-size: 5rem;
+        font-size: 3rem;
+        word-wrap: break-word;
+    }
+
+    .card>i {
+        font-size: 10rem;
     }
 
     .card {
         background-color: rgba(0, 0, 0, 0.5);
-        /* margin: 5vw; */
+        transition: all 0.5s ease;
     }
 </style>
